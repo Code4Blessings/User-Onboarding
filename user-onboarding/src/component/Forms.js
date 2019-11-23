ff48bbca4ce960e6f357deb425c27e0ed80da82c
Code4Blessings <robinwarden@code4blessings.com>
2019-11-22 22:06:55 -0500

    Added validation to form

--- user-onboarding/src/component/Forms.js
+++ user-onboarding/src/component/Forms.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './Forms.css';
 
-const Forms = () => {
+const Forms = ({values, errors, touched}) => {
     return (
         <div className="form-container">
         <Form>
             <h1>User Onboarding</h1>
             <label htmlFor="name">Name:
              <Field  type="text" name="name" placeholder="John Doe" />
+             {touched.name && errors.name && (<p className="errors">{errors.name}</p>)}
             </label>
 
             <label htmlFor="email">Email:
             <Field  type="text" name="email" placeholder="name@email.com" />
+            {touched.email && errors.email && (<p className="errors">{errors.email}</p>)}
             </label>
             
             <label htmlFor="password">Password:
             <Field  type="text" name="password" placeholder="password" />
+            {touched.password && errors.password && (<p className="errors">{errors.password}</p>)}
             </label>
 
-            <label className="checkbox-container" htmlFor="checkbox">Terms of Service?
+            <label className="checkbox-container" htmlFor="checkbox">Agree To Terms of Service?
             <Field type="checkbox" name="terms"/>
             <span className="checkmark" />
             </label>
@@ -37,7 +41,12 @@ const FormikForms = withFormik({
       password: password || "",
       terms: terms || "",
     }
-  }
+  },
+  validationSchema: Yup.object().shape({ 
+        name: Yup.string().required(), 
+        email: Yup.string().required(), 
+        password: Yup.string().required()
+}),
 })(Forms);
 
 export default FormikForms;
