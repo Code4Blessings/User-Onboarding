66863ff21770237f0d4e52cf9804e85ad1191de3
Code4Blessings <robinwarden@code4blessings.com>
2019-11-22 19:33:09 -0500

    Created user-onboarding form

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Forms from './component/Forms';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Forms />
     </div>
   );
 }
--- user-onboarding/src/component/Forms.js
+++ user-onboarding/src/component/Forms.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import './Forms.css';
 
 const Forms = () => {
     return (
-        <div>
-        
+        <div className="form-container">
+        <Form>
+            <h1>User Onboarding</h1>
+            <Field  type="text" name="name" placeholder="John Doe" />
+            <Field  type="text" name="email" placeholder="name@email.com" />
+            <Field  type="text" name="password" placeholder="password" />
+        </Form>
         </div>
     )
 }
+const FormikForms = withFormik({
+  mapPropsToValues({name, email, password }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+    }
+  }
+})(Forms);
+
+export default FormikForms;
