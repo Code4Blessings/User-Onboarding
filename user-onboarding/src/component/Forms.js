12b41252712cb1a74bc9d4f691c681434db890d8
Code4Blessings <robinwarden@code4blessings.com>
2019-11-22 20:56:20 -0500

    Added button and checkbox to form

--- user-onboarding/src/component/Forms.js
+++ user-onboarding/src/component/Forms.js
@@ -7,19 +7,35 @@ const Forms = () => {
         <div className="form-container">
         <Form>
             <h1>User Onboarding</h1>
-            <Field  type="text" name="name" placeholder="John Doe" />
+            <label htmlFor="name">Name:
+             <Field  type="text" name="name" placeholder="John Doe" />
+            </label>
+
+            <label htmlFor="email">Email:
             <Field  type="text" name="email" placeholder="name@email.com" />
+            </label>
+            
+            <label htmlFor="password">Password:
             <Field  type="text" name="password" placeholder="password" />
+            </label>
+
+            <label className="checkbox-container" htmlFor="checkbox">Terms of Service?
+            <Field type="checkbox" name="terms"/>
+            <span className="checkmark" />
+            </label>
+
+            <button type="submit">Submit</button>
         </Form>
         </div>
     )
 }
 const FormikForms = withFormik({
-  mapPropsToValues({name, email, password }) {
+  mapPropsToValues({name, email, password, terms }) {
     return {
       name: name || "",
       email: email || "",
       password: password || "",
+      terms: terms || "",
     }
   }
 })(Forms);
